b76f07cf183ea9b490b5cad73f5139f835e148af
shm <u7028158@anu.edu.au>
2020-12-12 15:30:22 +1100

    Added interface for usual functions

---
+++ FunctionInterface.py
@@ -0,0 +1,38 @@
+from header import *
+
+def Compute_min_max(tstart,tend,directory='./',N=200,field='rho'):
+    """
+    Routine to compute min/max of data
+        tstart: integer 
+            directory in which files reside
+        tend : integer 
+            timestep of file
+        directory : string
+            directory where files reside
+        N : integer
+            Resolution
+        field: string
+            Data field to read in
+        
+    """
+    directory = os.path.abspath(directory)
+    if(tend<tstart):
+        raise ValueError("tend has to be greater than tstart")
+    
+    if(tend>tstart):
+    #Compute min and max density
+        min_data = 1.e50
+        max_data = 1.e-50
+        for time in range(tstart,tend+1):
+            data = np.abs(read.readsinglefile(directory,time,N,field))
+            min_data = np.min(min_data,np.min(data))
+            max_data = np.max(max_data,np.max(data))
+
+    else:
+        min_data = np.min(data)
+        max_data = np.max(data)
+
+    return min_data,max_data
+
+
+
