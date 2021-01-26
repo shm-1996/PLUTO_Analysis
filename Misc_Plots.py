2cf91655e88be76b953c688bd3cccdc0722f55a7
shm <u7028158@anu.edu.au>
2021-01-26 20:29:34 +1100

    Added general slice plot functionality

--- Misc_Plots.py
+++ Misc_Plots.py
@@ -2,6 +2,14 @@ from header import *
 from FunctionInterface import *
 from Power_Spectra import *
 
+fields = np.array(['rho','vx1','vx2','vx3','prs','Tgas','ionx','iony','ueuv','urec',
+            'fun','bx1','bx2','bx3','phi'])
+
+labels = np.array(['$\\rho$',r'$v_{\mathrm{x}}$',r'$v_{\mathrm{y}}$',r'$v_{\mathrm{z}}$',
+r'$P_{\mathrm{th}}$',r'$T$',r'$x_{\mathrm{ion}}$',r'$y_{\mathrm{ion}}$',r'$u_{\mathrm{EUV}}$',
+r'$u_{\mathrm{REC}}$','fun',r'$B_{\mathrm{x}}$',r'$B_{\mathrm{y}}$',r'$B_{\mathrm{z}}$',
+r'$\phi$'])
+
 def Density_PDF(directory,tstart=100,tend=300,N=200,outdir=None):
     directory = os.path.abspath(directory)
     m = constant.mH_HydrogenMass
@@ -34,6 +42,64 @@ def Density_PDF(directory,tstart=100,tend=300,N=200,outdir=None):
 
     return
 
+
+def Slice_Plot(directory,field='rho',tstart=100,tend=300,N=200,outdir=None,log=True):
+
+    directory = os.path.abspath(directory)
+    m = constant.mH_HydrogenMass
+    k_boltzmann=constant.k_BoltzmannConstant
+    time = tstart
+
+    if(outdir is None):
+        outdir = directory + '/Slice_Plots/'
+    else:
+        outdir = os.path.abspath(outdir) + '/'
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    min_data,max_data = Compute_min_max(tstart,tend,directory,N,field)
+    if(log):
+        min_data = np.log10(min_data)
+        max_data = np.log10(max_data)
+
+    index = np.where(fields==field)[0]
+    label_plot = labels[index][0]
+    if(log):
+        label_plot = r"$\log_{10} \,$" +"{}".format(labels[index][0])
+        print(label_plot)
+
+    for time in range(tstart,tend+1):
+        data = read.readsinglefile(directory,time,N,field).reshape(N,N,N)
+        #Central slice
+        data = data[int(N/2)+1]
+
+        fig,axs = plt.subplots(ncols=1)
+        if(log):
+            im = axs.imshow(np.log10(data),vmin=min_data,vmax=max_data,extent=[0,4,0,4],
+                cmap='Spectral_r',origin='lower')
+        else:
+            im = axs.imshow(data,vmin=min_data,vmax=max_data,extent=[0,4,0,4],
+                cmap='Spectral_r',origin='lower')
+        axs.set_xticks([0.5,1.5,2.5,3.5])
+        axs.set_yticks([0.5,1.5,2.5,3.5])
+        axs.set_xlabel(r'$x\;$(pc)',labelpad=0.03)
+        axs.set_ylabel(r'$y\;$(pc)',labelpad=0.03)
+        time_sim = (time-tstart)*10
+        axs.annotate(r"$\mathbf{t} = \bf{%d}\;\mathrm{\mathbf{Kyr}}$"%time_sim,(0.2,3.7),
+            color='#3E403D',fontsize=12,weight='black',alpha=0.8)
+        cbar = fig.colorbar(im,ax = axs,shrink=1.0,use_gridspec=True,
+                        orientation='vertical',pad=0.02)
+        if(log):
+            cbar.ax.set_ylabel(label_plot,rotation=90,
+                       labelpad=5,fontsize=16)
+        plt.savefig(outdir+'{}_{:04d}'.format(field,time),bbox_inches='tight')
+        plt.clf()
+        plt.close(fig)
+
+    return
+
+
 def Initial_Analyses(directory,tstart=100,tend=300,N=200):
 
     print("Performing initial analyses for this simulation....")
@@ -44,7 +110,7 @@ def Initial_Analyses(directory,tstart=100,tend=300,N=200):
     plotColumnDensity(directory,tstart,tend,N)
 
     print("Making Movie")
-    Make_Movie(directory,tstart,tend,convert_pdfs=True)
+    Make_Movie(directory,'CombinedColumn',tstart,tend,convert_pdfs=True)
 
     print("Power spectra..")
     Compute_Power_Spectra(directory,tstart,tend,N)
